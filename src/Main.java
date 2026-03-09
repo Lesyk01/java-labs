fc74b2eef44edcd3156402f121d2c4c3386c03f8
Lesyk01 <olexua10@gmail.com>
2026-03-09 13:52:00 +0300

    Translate output messages to English

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,32 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("--- ЗАВДАННЯ 1 ---");
+        System.out.println("--- task 1 ---");
         int a = 15;
         int b = 4;
 
         int divisionResult = a / b;
         int remainder = a % b;
 
-        System.out.println("Результат ділення: " + divisionResult);
-        System.out.println("Залишок від ділення: " + remainder);
+        System.out.println("Division result: " + divisionResult);
+        System.out.println("Remainder of division:" + remainder);
 
 
-        System.out.println("--- ЗАВДАННЯ 2 ---");
+        System.out.println("--- Taks 2 ---");
         int number = 72;
 
         int firstDigit = number / 10;
         int secondDigit = number % 10;
         int sum = firstDigit + secondDigit;
 
-        System.out.println("Сума цифр числа " + number + " дорівнює: " + sum);
+        System.out.println("Sum of the digits of a number" + number + " = " + sum);
 
 
-        System.out.println("--- ЗАВДАННЯ 3 ---");
+        System.out.println("--- Task 3  ---");
         double myNumber = 8.7;
 
         int roundedNumber = (int) Math.round(myNumber);
 
-        System.out.println("Округлене число: " + roundedNumber);
+        System.out.println("Rounded number: " + roundedNumber);
     }
 }
